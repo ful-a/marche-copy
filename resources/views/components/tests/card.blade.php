1cf45abb7e87e13d0e55a96f46c66b126525aab9
ful-a <croqkes@docomo.ne.jp>
2022-01-14 20:27:06 +0900

    fix compornent with attributes of class

--- resources/views/components/tests/card.blade.php
+++ resources/views/components/tests/card.blade.php
@@ -3,7 +3,9 @@
   'content' => 'コンテンツの初期値',
   'message' => 'メッセージの初期値',
 ])
-<div class="border-2 shadow-md p-2 w-1/4">
+<div {{ $attributes->merge([
+  'class' => 'border-2 shadow-md p-2 w-1/4'
+  ]) }}>
   <div>{{ $title }}</div>
   <div>画像</div>
   <div>{{ $content }}</div>
--- resources/views/test/component-test1.blade.php
+++ resources/views/test/component-test1.blade.php
@@ -10,5 +10,6 @@
 <x-tests.card title="タイトル" content="本文" :message="$controllMessage" />
 
 <!-- propsで初期値を設定したコンポーネント -->
-<x-tests.card title="タイトル２" />
+<x-tests.card title="タイトル2" />
+<x-tests.card title="タイトル3:cssの変更" class="bg-red-300" />
 </x-tests.app>
