b3ec0259bd25e275ab3f34e58b8edec842b0b7bd
ful-a <croqkes@docomo.ne.jp>
2022-01-14 14:57:02 +0900

    add slot for components

--- resources/views/test/component-test1.blade.php
+++ resources/views/test/component-test1.blade.php
@@ -1,1 +1,8 @@
-コンポーネントテスト１
+<!-- コンポーネントの指定 -->
+<!-- resorce/views/componentsから見たパス -->
+
+<!-- x- tests/app.blade.php -->
+<x-tests.app>
+  <!-- {{ $slot }}の中に値が入る -->
+  コンポーネントテスト１
+</x-tests.app>
